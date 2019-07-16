4cc819ba1a49b94ad2633a37587617fdcf215a50
Angel Perez <iAngel.p93@gmail.com>
2019-07-16 05:35:27 -0400

    Define Event#highlights scope to randomly select events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    @highlights = Event.highlights
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,8 @@ class Event < ApplicationRecord
 
   default_scope { order(event_date: :desc) }
 
+  scope :highlights, -> { order(Arel.sql("RANDOM()")) }
+
   validates :title, presence: true
   validates :event_date, presence: true
 
