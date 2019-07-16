e65916aae827d755194847ca640edf87216fdb50
Angel Perez <iAngel.p93@gmail.com>
2019-07-16 06:28:43 -0400

    Create Event seeds for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+events = Event.create([
+  {
+    title: 'Electronic Circus',
+    location: 'Berlin, Germany',
+    description: 'A party full of glitter, confetti, and beats.',
+    event_date: Date.today,
+    price: 20.00,
+    event_time: Time.now
+  },
+
+  {
+    title: 'Housewerk',
+    location: 'Budapest, Hungary',
+    description: "They know what house is... But they don't know what is house. Everybody shout.",
+    event_date: Date.yesterday,
+    price: 25.00,
+    event_time: Time.now
+  },
+
+  {
+    title: 'Lowkey Tech',
+    location: 'Prague, Czech Republic',
+    description: 'Chill techno and tech house.',
+    event_date: 2.days.ago,
+    price: 18.99,
+    event_time: Time.now
+  }
+])
+
+events.map { |e| e.save! }
