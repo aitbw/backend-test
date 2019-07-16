a22d23c711578f8024d4548d80d7049d6a66d522
Angel Perez <iAngel.p93@gmail.com>
2019-07-16 06:30:20 -0400

    Improve Event#highlights scope by limiting results to 5

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
 
   default_scope { order(event_date: :desc) }
 
-  scope :highlights, -> { order(Arel.sql("RANDOM()")) }
+  scope :highlights, -> { order(Arel.sql("RANDOM()")).limit(5) }
 
   validates :title, presence: true
   validates :event_date, presence: true
